4c6f18e730a0a813c6b69ef4f815228b194d71be
skoh-fley <qhiiyr@gmail.com>
2013-04-09 15:45:22 -0700

    Added an ability to recompile ycm_core for YouCompleteMe

--- vim-plugin-setup.py
+++ vim-plugin-setup.py
@@ -6,22 +6,32 @@ import sys
 
 print(
 """vim-plugin-setup: What would you like to do?
-  1. Init and update plugins
+  1. Init plugins
   2. Pull each plugin
   3. Add a plugin from GitHub
   q. Quit"""
 )
 cin = input("Enter an option: ")
 
+# Quit
 if cin is "q":
   sys.exit()
 
-
+# Init plugins
 if cin is "1":
   subprocess.call(["git", "submodule", "init"])
   subprocess.call(["git", "submodule", "update"])
+
+# Pull each plugin
 if cin is "2":
+  if os.path.exists(".vim/bundle/YouCompleteMe"):
+    ycm_core = input("Also recompile ycm_core for YouCompleteMe? [y/N] ")
   subprocess.call(["git", "submodule", "foreach", "git", "pull", "origin", "master"])
+  if ycm_core.lower() is 'y':
+    os.chdir(".vim/bundle/YouCompleteMe")
+    subprocess.call(["./install.sh"])
+
+# Add a plugin from GitHub
 if cin is "3":
   if not os.path.exists(".vim"):
     print("To add a plugin, you must run this script from the directory containing your .vim folder.")
