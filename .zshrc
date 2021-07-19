ed1374f3e3ba7b4c07bbabec2b0c3f95f5a8cc59
glacials <qhiiyr@gmail.com>
2021-07-18 21:17:05 -0700

    Move from Makefile to shell script

--- .zshrc
+++ .zshrc
@@ -1,25 +1,27 @@
 # oh-my-zsh setup
-export ZSH=/Users/glacials/.oh-my-zsh
+export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="pygmalion"
 DISABLE_AUTO_UPDATE="true"
 plugins=(bundler brew common-aliases dircycle encode64 gem osx rails ruby vi-mode zeus zsh-iterm-touchbar)
 source $ZSH/oh-my-zsh.sh
 
+path+=("/usr/local/opt/node@6/bin")
+path+=("$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin")
+path+=("/home/linuxbrew/.linuxbrew/bin")
+
 # run history-substituted commands (e.g. containing `!!` or `!$`) without confirmation
 unsetopt histverify
 
 alias vi=vim
-alias vim=nvim
-alias vimdiff='nvim -d'
+alias vimdiff='vim -d'
 alias gs='git status'
 alias rebase='export CURRENT_BRANCH=`git name-rev --name-only HEAD` && git checkout master && git pull && git checkout `echo $CURRENT_BRANCH` && git rebase master'
 alias lock='/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine'
-unalias rm
 
 # general env vars
-export EDITOR=nvim
+export EDITOR=vim
 export GPG_TTY=$(tty)
-export AWS_DEFAULT_REGION=us-west-2
+export AWS_DEFAULT_REGION=us-east-1
 
 # ruby
 export PATH=~/.rbenv/shims:$PATH
@@ -29,30 +31,11 @@ eval "$(rbenv init -)"
 export GOPATH=~/go
 export PATH=$GOPATH/bin:$PATH
 
-# gpg (from https://gist.github.com/bmhatfield/cc21ec0a3a2df963bffa3c1f884b676b)
-GPG_TTY=$(tty)
-export GPG_TTY
-if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
-    source ~/.gnupg/.gpg-agent-info
-    export GPG_AGENT_INFO
-else
-    eval $(gpg-agent --daemon ~/.gnupg/.gpg-agent-info)
-fi
-
 # fun things at shell boot
 fortune ~/pj/dotfiles/fortunes | cowsay -n
 source ~/.profile
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-export PATH="$PATH:/usr/local/opt/node@6/bin"
-
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # direnv
 eval "$(direnv hook zsh)"
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/glacials/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/glacials/Downloads/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/Users/glacials/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/glacials/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
---
+++ install.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -euo pipefail
+set -x # uncomment to print all commands as they happen
+
+uname=$(uname -s)
+pwd=$(pwd)
+
+apt="sudo apt-get --quiet --quiet --assume-yes"
+brew="brew"
+brewinstall="$brew install --quiet"
+npm="npm --silent"
+
+# This entire file is idempotent! It is safe to re-run at any time.
+#
+# If you're setting up a machine for the first time, you should do the following manual steps after running this file:
+#
+# 1. gpg --gen-key
+# 2. In the output note the key under the line starting with "pub" (you can re-access this later with gpg --list-keys).
+# 3. git config --global user.signingkey KEY_NOTED_FROM_ABOVE
+# 4. gpg --armor --export | pbcopy
+# 5. Paste that in https://github.com/settings/gpg/new
+#
+# (partly from https://gist.github.com/bmhatfield/cc21ec0a3a2df963bffa3c1f884b676b)
+
+### Start symlinks ###
+echo "Setting up symbolic links."
+
+
+# Symlinks
+# Note: If creating a symlink to something in a subdirectory of ~, first mkdir -p that directory.
+mkdir -p $HOME/.config
+[ -h $HOME/.ackrc ]                && ln -fs $(pwd)/.ackrc                $HOME         || ln -is $(pwd)/.ackrc                $HOME
+[ -h $HOME/.amethyst ]             && ln -fs $(pwd)/.amethyst             $HOME         || ln -is $(pwd)/.amethyst             $HOME
+[ -h $HOME/.config/nvim ]          && ln -fs $(pwd)/.config/nvim          $HOME/.config || ln -is $(pwd)/.config/nvim          $HOME/.config
+[ -h $HOME/.gitconfig ]            && ln -fs $(pwd)/.gitconfig            $HOME         || ln -is $(pwd)/.gitconfig            $HOME
+[ -h $HOME/.gitignore_global ]     && ln -fs $(pwd)/.gitignore_global     $HOME         || ln -is $(pwd)/.gitignore_global     $HOME
+
+### End symlinks ###
+
+### Start package managers ###
+echo "Setting up package managers."
+
+$apt update
+$apt upgrade
+$apt install zsh
+
+# Linux
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Update path here since we can't source .zshrc yet; we haven't installed every tool it will invoke.
+export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+
+$apt install -y build-essential # Homebrew asks for this on install
+$brewinstall gcc # Homebrew asks for this on install
+$brew update
+$brew upgrade
+echo "Ignore above messages re: gcc and build-essential; it is done."
+
+### End package managers ###
+
+### Start languages ###
+echo "Setting up languages."
+
+# JavaScript
+$brewinstall npm
+$npm install -g typescript bower
+
+# Python
+$brewinstall pyenv
+
+# Ruby
+$brewinstall rbenv ruby-build
+
+### End languages ###
+
+### Start application installations ###
+echo "Starting application installations."
+
+$brewinstall direnv
+
+### End application installations ###
+
+
+# Fortune
+$brewinstall fortune cowsay
+./fortunes/strfile
+
+# Create $HOME/.profile (to put secret things, that shouldn't go in this repository)
+touch $HOME/.profile
+
+### Start shell configuration
+
+# Install oh-my-zsh
+rm -rf $HOME/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --unattended)"
+rm -rf $HOME/.oh-my-zsh
+[ -h $HOME/.oh-my-zsh ] && ln -fs $(pwd)/.oh-my-zsh $HOME || ln -is $(pwd)/.oh-my-zsh $HOME
+
+# Change to zsh and replace session
+chsh -s /bin/zsh `whoami`
+rm -f $HOME/.zshrc
+[ -h $HOME/.zshrc ] && ln -fs $(pwd)/.zshrc $HOME || ln -is $(pwd)/.zshrc $HOME
+zsh
+
+### End shell configuration
