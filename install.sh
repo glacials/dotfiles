d5e7ece220e56b5d0fb2743d52485a58e12bb609
glacials <qhiiyr@gmail.com>
2021-07-19 09:29:00 -0700

    Add runtime dependencies of pyenv

--- install.sh
+++ install.sh
@@ -66,6 +66,11 @@ $brewinstall npm
 $npm install -g typescript bower
 
 # Python
+#   Runtime dependencies of pyenv (https://github.com/pyenv/pyenv/wiki#suggested-build-environment)
+$apt install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+#   pyenv
 $brewinstall pyenv
 
 # Ruby
