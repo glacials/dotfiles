0c0ab64529568bafbdba0ccee4df7e38a447dff9
glacials <ben@twos.dev>
2022-12-24 18:26:33 -0800

    Add .config/emacs/init.el

---
+++ dot_config/emacs/init.el
@@ -0,0 +1,10 @@
+(when (>= emacs-major-version 24)
+  (progn
+    ;; load emacs 24's package system. Add MELPA repository.
+    (require 'package)
+    (add-to-list
+     'package-archives
+     '("melpa" . "https://melpa.org/packages/")
+     t))
+
+  (when (< emacs-major-version 27) (package-initialize)))
