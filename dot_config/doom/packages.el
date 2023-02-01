2a2c6518d8b24b481c9ad017cd3f5d65b91cfae3
glacials <ben@twos.dev>
2023-02-01 14:43:10 -0800

    Update .config/doom/packages.el

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 
 (package! hcl-mode)
+(package! org-journal) ; Daily org-mode entries automatically
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
