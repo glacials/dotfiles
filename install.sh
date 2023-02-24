1c38dfc6be3497e4747316547bb935760c4ca41b
glacials <ben@twos.dev>
2023-02-23 20:18:59 -0800

    Add some fonts

--- install.sh
+++ install.sh
@@ -142,44 +142,46 @@ sh ~/.config/fortune/strfile
 # Services
 brew services start syncthing
 
-# Non-App Store GUI apps (only if not installed; casks don't skip automatically)
-brew list 1password     >/dev/null || $brewinstall 1password
-brew list discord       >/dev/null || $brewinstall discord
-brew list docker        >/dev/null || $brewinstall homebrew/cask/docker
-brew list firefox       >/dev/null || $brewinstall firefox
-brew list google-chrome >/dev/null || $brewinstall google-chrome
-brew list iterm2        >/dev/null || $brewinstall iterm2
-brew list kitty         >/dev/null || $brewinstall kitty
-brew list launchcontrol >/dev/null || $brewinstall launchcontrol
-brew list stay          >/dev/null || $brewinstall stay
+# Casks (casks still install even if already installed, so we skip here if so)
+brew list 1password           >/dev/null || $brewinstall 1password
+brew list discord             >/dev/null || $brewinstall discord
+brew list docker              >/dev/null || $brewinstall homebrew/cask/docker
+brew list firefox             >/dev/null || $brewinstall firefox
+brew list font-hack-nerd-font >/dev/null || $brewinstall homebrew/cask-fonts/font-hack-nerd-font
+brew list font-jetbrains-mono >/dev/null || $brewinstall homebrew/cask-fonts/font-jetbrains-mono
+brew list google-chrome       >/dev/null || $brewinstall google-chrome
+brew list iterm2              >/dev/null || $brewinstall iterm2
+brew list kitty               >/dev/null || $brewinstall kitty
+brew list launchcontrol       >/dev/null || $brewinstall launchcontrol
+brew list stay                >/dev/null || $brewinstall stay
 
 # App Store apps
 if [[ $uname == darwin ]]; then
-  $masinstall 408981434  # iMovie
-  $masinstall 409183694  # Keynote
-  $masinstall 409201541  # Pages
-  $masinstall 409203825  # Numbers
-  $masinstall 411643860  # DaisyDisk
-  $masinstall 430798174  # HazeOver
-  $masinstall 441258766  # Magnet
-  $masinstall 497799835  # Xcode
-  $masinstall 747648890  # Telegram
-  $masinstall 775737590  # iA Writer
-  $masinstall 883878097  # Server
-  $masinstall 920404675  # Monodraw
-  $masinstall 1085114709 # Parallels Desktop
-  $masinstall 1233861775 # Acorn
-  $masinstall 1246969117 # Steam Link
-  $masinstall 1451544217 # Adobe Lightroom
-  $masinstall 1475387142 # Tailscale
-  $masinstall 1478821913 # GoLinks for Safari
-  $masinstall 1480068668 # Messenger
-  $masinstall 1480933944 # Vimari
-  $masinstall 1482454543 # Twitter
-  $masinstall 1507890049 # Pixeur
-  $masinstall 1518425043 # Boop
-  $masinstall 1534275760 # LanguageTool
-  $masinstall 1569813296 # 1Password for Safari
+		$masinstall 408981434  # iMovie
+		$masinstall 409183694  # Keynote
+		$masinstall 409201541  # Pages
+		$masinstall 409203825  # Numbers
+		$masinstall 411643860  # DaisyDisk
+		$masinstall 430798174  # HazeOver
+		$masinstall 441258766  # Magnet
+		$masinstall 497799835  # Xcode
+		$masinstall 747648890  # Telegram
+		$masinstall 775737590  # iA Writer
+		$masinstall 883878097  # Server
+		$masinstall 920404675  # Monodraw
+		$masinstall 1085114709 # Parallels Desktop
+		$masinstall 1233861775 # Acorn
+		$masinstall 1246969117 # Steam Link
+		$masinstall 1451544217 # Adobe Lightroom
+		$masinstall 1475387142 # Tailscale
+		$masinstall 1478821913 # GoLinks for Safari
+		$masinstall 1480068668 # Messenger
+		$masinstall 1480933944 # Vimari
+		$masinstall 1482454543 # Twitter
+		$masinstall 1507890049 # Pixeur
+		$masinstall 1518425043 # Boop
+		$masinstall 1534275760 # LanguageTool
+		$masinstall 1569813296 # 1Password for Safari
 fi
 ########################################## End application installations
 
