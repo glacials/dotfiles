447a3313d2131986f7d3deafa81ae1ca4895cb8f
glacials <ben@twos.dev>
2023-03-06 04:49:43 +0100

    work

--- run_once_install_chezmoi.sh
+++ run_once_install_chezmoi.sh
@@ -18,7 +18,7 @@ test -z ${DEBUG:-} || set -x
 cdn="https://raw.githubusercontent.com/glacials/dotfiles/main"
 f="functions.sh"
 d="$(dirname $0)"
-test -f $d/$f && . $d/$f || . <(curl -s $cdn/$f)
+test -f $d/$f && . $d/$f || . <(curl -s "$cdn/$f")
 
 install_package_now "chezmoi"
 chezmoi init --apply glacials
