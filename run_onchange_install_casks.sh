b32142bef44229cd8281fc3f4237709a1d4c6060
glacials <ben@twos.dev>
2023-03-13 12:06:03 -0700

    Fix font installs

--- run_onchange_install_casks.sh
+++ run_onchange_install_casks.sh
@@ -11,18 +11,19 @@ test -z ${DEBUG:-} || set -x
 function cask() {
 		# brew install on a cask installs even if alrady installed, so we'll skip
 		# that ourselves if that's the case.
-		brew list "$1" >/dev/null || casks="${casks:-} $1"
-}
 
-brew tap homebrew/cask-fonts # For font-* casks below
+		# Strip the tap name; `brew list` doesn't want it
+		pkg=$(echo "$1" | sed -e 's/.*\///')
+		brew list "$pkg" >/dev/null || casks="${casks:-}$1 "
+}
 
 cask 1password # Password manager
 cask discord # Chat app
 cask docker # Containerization
 cask firefox # Browser
-cask font-hack-nerd-font # Nerd fonts
-cask font-jetbrains-mono # JetBrains Mono font
-cask font-source-code-pro # Source Code Pro font
+cask homebrew/cask-fonts/font-hack-nerd-font # Nerd fonts
+cask homebrew/cask-fonts/font-jetbrains-mono # JetBrains Mono font
+cask homebrew/cask-fonts/font-source-code-pro # Source Code Pro font
 cask google-chrome # Browser
 cask iterm2 # Terminal emulator
 cask kitty # Terminal emulator
@@ -35,5 +36,5 @@ cask vlc # Video player
 
 # Install all casks at once, to avoid the overhead of multiple invocations/
 if ! test -z "${casks:-}"; then
-		brew install --force --quiet "${casks:-}"
+		brew install --force --quiet ${casks}
 fi
