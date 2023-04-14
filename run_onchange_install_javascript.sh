cb458bc68fc674f4bb2858447df9affd70f2af43
Benjamin Carlsson <ben@twos.dev>
2023-04-14 00:59:24 -0700

    Update run_onchange_install_javascript.sh

--- run_onchange_install_javascript.sh
+++ run_onchange_install_javascript.sh
@@ -2,13 +2,19 @@
 
 set -euo pipefail
 test -z ${DEBUG:-} || set -x
+uname=$(uname -s | tr "[:upper:]" "[:lower:]")
 
 # This script installs JavaScript and several tools in the JavaScript ecosystem.
 echo "Installing JavaScript and friends"
 
 . $(chezmoi source-path)/functions.sh
 
-install_now nodenv npm
+if [[ $uname == linux ]]; then
+	curl -fsSL https://github.com/nodenv/nodenv-installer/raw/HEAD/bin/nodenv-installer | bash
+else
+	install_now nodenv
+fi
+install_now npm
 
 latest=$(nodenv install --list 2>/dev/null | sed -n '/^[[:space:]]*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}[[:space:]]*$/ h;${g;p;}')
 nodenv install --skip-existing $latest
