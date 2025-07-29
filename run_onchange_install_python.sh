cfaf7f42ba3c3d9d9497c6097a5afbb16f1f03ee
Benjamin Carlsson <bc@openai.com>
2025-07-29 11:38:19 -0700

    Update run_onchange_install_python.sh

--- run_onchange_install_python.sh
+++ run_onchange_install_python.sh
@@ -10,25 +10,11 @@ echo "Installing Python and friends"
 . $(chezmoi source-path)/functions.sh
 
 if [[ $uname == darwin ]]; then
-    # Runtime dependencies of pyenv (https://github.com/pyenv/pyenv/wiki#suggested-build-environment)
-    pkginstall make build-essential libssl-dev zlib1g-dev \
-         libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-         libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-		if [[ -d "/home/linuxbrew" ]]; then
-        linuxbrew_home="/home/linuxbrew/.linuxbrew"
-    else
-        linuxbrew_home="$HOME/.linuxbrew"
-    fi
-    export LDFLAGS="-L$linuxbrew_home/opt/openssl@3/lib"
-    export CPPFLAGS="-I$linuxbrew_home/opt/openssl@3/include"
-
+	# Runtime dependencies of pyenv (https://github.com/pyenv/pyenv/wiki#suggested-build-environment)
+	pkginstall make build-essential libssl-dev zlib1g-dev \
+	libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+ 
 	npm install -g pyright # Language server
 	install_now pyenv pyenv-virtualenv
-	latest=$(\
-      pyenv install --list 2>/dev/null | \
-      sed -n '/^[[:space:]]*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}[[:space:]]*$/ h;${g;p;}' \
-    )
-	pyenv install --skip-existing $latest
-	pyenv global $latest
 fi
