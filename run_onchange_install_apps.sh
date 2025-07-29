0750ce515a41e1f9f0c2d7be1427f843baf3b61d
Benjamin Carlsson <bc@openai.com>
2025-07-29 11:34:49 -0700

    Update run_onchange_install_apps.sh

--- run_onchange_install_apps.sh
+++ run_onchange_install_apps.sh
@@ -30,22 +30,16 @@ function app() {
     fi
 }
 
-# app 408981434  # iMovie -- For some reason iMovie always reinstalls. Have
-                 # Final Cut Pro now anyways so not super important.
 app 409183694  # Keynote
 app 409201541  # Pages
 app 409203825  # Numbers
 app 409907375  # Apple Remote Desktop
 app 411643860  # DaisyDisk
 app 424389933  # Final Cut Pro
-app 430798174  # HazeOver
-app 441258766  # Magnet
 app 497799835  # Xcode
 app 668208984  # GIPHY Capture
-app 669289700  # Sweet Home 3D
 app 747648890  # Telegram
 app 775737590  # iA Writer
-app 883878097  # Server
 app 920404675  # Monodraw
 app 1046095491 # Freeze - for Amazon Glacier
 app 1085114709 # Parallels Desktop
@@ -53,20 +47,14 @@ app 1118136179 # AutoMute
 app 1147396723 # WhatsApp Desktop
 app 1233861775 # Acorn
 app 1246969117 # Steam Link
-app 1451544217 # Adobe Lightroom
 app 1475387142 # Tailscale
 app 1478821913 # GoLinks for Safari
 app 1480068668 # Messenger
 app 1480933944 # Vimari
-# app 1482454543 # Twitter
 app 1507890049 # Pixeur
 app 1518425043 # Boop
 app 1534275760 # LanguageTool
 app 1569813296 # 1Password for Safari
-
-if (( $macosversion > 12 )); then
-    app 6444602274 # Ivory
-fi
 
 if ! test -z "${appids_to_install:-}"; then
     mas install $appids_to_install
